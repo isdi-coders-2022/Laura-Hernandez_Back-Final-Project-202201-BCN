ae45dd8ee8f3bdc1d5f6d6e3211d2bffb419ce34
Laura Hernández <lhvarela@gmail.com>
2022-03-07 16:39:41 +0100

    Added test to errors file

---
+++ src/middlewares/errors.test.js
@@ -0,0 +1,66 @@
+const { notFoundError, generalError } = require("./errors");
+
+describe("Given a notFoundError function", () => {
+  describe("When it receives a request", () => {
+    test("Then it should call the method json with an error", () => {
+      const mockRes = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
+
+      const mockedRes = mockRes();
+
+      notFoundError(null, mockedRes);
+
+      expect(mockedRes.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a generalError function", () => {
+  describe("When it receives an error and a response", () => {
+    test("Then it should call method json", () => {
+      const mockRes = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
+
+      const error = {
+        message: "error",
+        code: 500,
+      };
+
+      const mockedRes = mockRes();
+
+      generalError(error, null, mockedRes, null);
+
+      expect(mockedRes.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives an error without status and a response", () => {
+    test("Then it should call method json", () => {
+      const mockRes = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res);
+        res.json = jest.fn().mockReturnValue(res);
+        return res;
+      };
+
+      const error = {
+        message: "error",
+        code: null,
+      };
+
+      const mockedRes = mockRes();
+
+      generalError(error, null, mockedRes, null);
+
+      expect(mockedRes.json).toHaveBeenCalled();
+    });
+  });
+});
