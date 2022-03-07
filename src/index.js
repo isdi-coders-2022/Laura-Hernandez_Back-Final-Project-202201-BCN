5a4f094be05d9ab60424a6b64ce74a39c09ee71a
Laura Hernández <lhvarela@gmail.com>
2022-03-07 16:25:17 +0100

    Added import serverUp in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("skybuzz:root");
 const app = require("./server/index");
+const serverUp = require("./server/serverUp");
 
 const port = process.env.PORT || 4000;
 
