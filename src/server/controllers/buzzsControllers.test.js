d80a0b94f24a6417aab318eea569d71e505e95b2
Laura Hernández <lhvarela@gmail.com>
2022-03-10 14:26:52 +0100

    Added test to deleteBuzz controller

--- src/server/controllers/buzzsControllers.test.js
+++ src/server/controllers/buzzsControllers.test.js
@@ -1,5 +1,5 @@
 const Buzz = require("../../db/models/Buzz");
-const { getAllBuzzs } = require("./buzzsControllers");
+const { getAllBuzzs, deleteBuzz } = require("./buzzsControllers");
 
 describe("Given an getAllBuzzs controller", () => {
   beforeEach(() => {
@@ -28,6 +28,41 @@ describe("Given an getAllBuzzs controller", () => {
 
       expect(Buzz.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ buzzs });
+    });
+  });
+});
+
+describe("Given an deleteBuzz controller", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  describe("When it receives an existing id to delete", () => {
+    test("Then it should call method json with an empty buzz", async () => {
+      const req = {
+        params: {
+          id: "12345",
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+      const buzzToDelete = {
+        id: "12345",
+        category: "General",
+        likes: 0,
+        comments: [],
+        author: "Madonna",
+        text: "Madonna is writting a message",
+        date: "2022-03-08T18:11:03.390Z",
+      };
+
+      Buzz.findByIdAndRemove = jest.fn().mockResolvedValue(buzzToDelete);
+
+      await deleteBuzz(req, res, next);
+
+      expect(res.json).toHaveBeenCalledWith({});
+      expect(next).not.toHaveBeenCalled();
     });
   });
 });
