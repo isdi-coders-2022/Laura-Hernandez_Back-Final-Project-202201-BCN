c14a2b2e91f82cde226a9220d7fe82141e8eb074
Laura Hernández <lhvarela@gmail.com>
2022-03-14 14:19:08 +0100

    Deleted alphanumeric in text validation Joi

--- src/server/controllers/buzzValidation/buzzValidator.js
+++ src/server/controllers/buzzValidation/buzzValidator.js
@@ -1,8 +1,8 @@
 const { Joi } = require("express-validation");
 
 const validationBuzzJoi = Joi.object({
-  author: Joi.string().alphanum().min(3).max(15).required(),
-  text: Joi.string().alphanum().min(3).max(200).required(),
+  author: Joi.string().min(3).max(15).required(),
+  text: Joi.string().min(3).max(200).required(),
   topic: Joi.string().default("general"),
   date: Joi.date().default(new Date()),
   likes: Joi.number().default(0),
