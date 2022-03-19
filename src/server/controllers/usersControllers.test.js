fd8a4443e179e3925b1b910f3c79adab64570934
Laura Hernández <lhvarela@gmail.com>
2022-03-19 18:41:15 +0100

    Added registerUser controller test

--- src/server/controllers/usersControllers.test.js
+++ src/server/controllers/usersControllers.test.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const chalk = require("chalk");
 const jwt = require("jsonwebtoken");
 const User = require("../../db/models/User");
-const { loginUser } = require("./usersControllers");
+const { loginUser, registerUser } = require("./usersControllers");
 
 jest.mock("../../db/models/User");
 
@@ -86,6 +86,33 @@ describe("Given an loginUser controller", () => {
 
       expect(User.findOne).toHaveBeenCalled();
       expect(next).toHaveBeenCalledWith(error);
+    });
+  });
+});
+
+describe("Given an registerUser controller", () => {
+  describe("When it receives a request with a new username", () => {
+    test("Then it should call method json and respond with a 201 status", async () => {
+      const req = {
+        body: { name: "Rosa", username: "Rosa0", password: "Rosa1234" },
+      };
+      const res = {
+        json: jest.fn(),
+        status: jest.fn(),
+      };
+      const next = jest.fn();
+      const user = {
+        name: "Rosa",
+        username: "Rosa0",
+        password:
+          "$2b$10$ZZ7dKrL4q2zWFAN8CnCWEOnWPQKQAfjbqvg8yMHnaw4DUXxUQSvae",
+      };
+
+      User.findOne = jest.fn().mockResolvedValue(false);
+      User.create = jest.fn().mockResolvedValue(user);
+      await registerUser(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(201);
     });
   });
 });
