401dd4729c53d8c8d4f8c00563e485c97bd00622
Laura Hernández <lhvarela@gmail.com>
2022-03-21 09:14:23 +0100

    Changed getAuthor function in utils folder

---
+++ src/utils/getAuthor.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const getAuthor = (headerAuth) => {
+  const userToken = headerAuth.replace("Bearer ", "");
+  const payload = jwt.decode(userToken, process.env.JWT_SECRET);
+  return payload.id;
+};
+
+export default getAuthor;
